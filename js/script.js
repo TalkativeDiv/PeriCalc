79c1faa8103429e465d701d11e73b49f4a221dd4
TalkativeDiv <85126640+TalkativeDiv@users.noreply.github.com>
2021-11-05 18:37:38 -0400

    formatted files + fixed formulas

--- js/script.js
+++ js/script.js
@@ -2,32 +2,26 @@
 ---- Area Calculator ----
 
 ? Info:
-? This program will calculate the area of a square 
+? This program will calculate the area of a square and the perimeter of a square
 
 * Code Summary:
 * We first grab the Elements from the DOM
-* We then register a event listener that fires
-* when the button is clicked and have a callback 
-* function that fires when the button is clicked
-* which calculates the area & perimeter
+* We then register a event listener that that listens for
+* when the button is clicked then we calculate the area & perimeter
 */
-
+// DOM Elements
 const button = document.querySelector('button');
 const input = document.querySelector('input')
 const lblArea = document.querySelector('.lbl-area')
 const lblPerimeter = document.querySelector('.lbl-perimeter')
 const loader = document.querySelector(".loader");
-
+// Button Click
 button.addEventListener('click', () => {
-let area = input.value * input.value;
-let perimeter = input.value * 4;
-lblArea.innerText = `A r e a : ${area}`
-lblPerimeter.innerText = `P e r i m e t e r : ${perimeter}`
+    let area = input.value * input.value;
+    let perimeter = input.value * 4;
+    lblArea.innerText = `A r e a : ${area}`
+    lblPerimeter.innerText = `P e r i m e t e r : ${perimeter}`
 })
 
 // Preloader
-window.addEventListener("load", function () {
-     this.setTimeout(() => {
-       loader.className += " hidden";
-     }, 0) // class "loader hidden"
-   });
+window.addEventListener("load", loader.className += " hidden");
